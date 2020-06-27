d42c01c19ab4036a76113d491c9fd604219c8fb2
Paul K George <paulkottalil20@gmail.com>
2020-06-28 02:44:37 +0530

    added reducers

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+
 
 export default function AddTransaction() {
+    
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+
+    const {addTransaction} = useContext(GlobalContext)
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        const newTransaction = {
+            id :  Math.floor(Math.random()*10000),
+            amount : parseInt(amount),
+            text,
+            
+        }
+
+        addTransaction(newTransaction)
+        setAmount(0)
+        setText('')
+
+    }
     return (
         <div>
             <h3>Add new transaction</h3>
-            <form id="form">
+            <form id="form" onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange = {(e) => setText(e.target.value)} placeholder="Enter text..." />
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 
 export default function Transaction({transaction}) {
+    const {deleteTransaction} = useContext(GlobalContext)
     return (
         <li className={transaction.amount<0?'minus':'plus'}>
             {transaction.text} 
             <span>{transaction.amount < 0?'-':'+'}
                 ${Math.abs(transaction.amount)}</span>
-            <button className="delete-btn">x</button>
+            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
